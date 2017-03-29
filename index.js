cb3211cc0577678b0ffc65cb03b4c99546b1c359
jxom <jakemoxey@gmail.com>
2017-03-29 23:24:23 +1100

    Added recent history as touch bar buttons

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const { TouchBar } = require('electron');
+const shellHistory = require('shell-history');
 
-const TOUCH_BAR_CONTROLS = {
+const INITIAL_TOUCH_BAR_CONTROLS = {
   default: [
-    { label: 'clear', bgColor: '#c0392b', command: 'clear' },
-    { label: 'ls -la', bgColor: '#2980b9', command: 'ls -la' },
-    { label: 'top', bgColor: '#2980b9', command: 'top' },
-    { label: 'history', bgColor: '#2980b9', command: 'history' }
+    { type: 'button', payload: { label: 'clear', bgColor: '#c0392b', command: 'clear' } },
+    { type: 'button', payload: { label: 'ls -la', bgColor: '#2980b9', command: 'ls -la' } }
   ]
+};
+
+const getCommandHistoryControls = length => {
+  let history = shellHistory();
+  history = history.slice(history.length - length, history.length);
+  history = history.reverse();
+  return history.map(item => ({ type: 'button', payload: { label: `${item}`, bgColor: '#7f8c8d', command: `${item}` } }));
+};
+
+const getTouchBarControls = view => {
+  let controls = INITIAL_TOUCH_BAR_CONTROLS[view];
+  if (view === 'default') {
+    controls = [...controls, ...getCommandHistoryControls(5)];
+  }
+  return controls;
 };
 
 let currentUid;
@@ -22,22 +36,34 @@ const waitFor = (object, key, fn) => {
 exports.onWindow = win => {
   const { TouchBarButton } = TouchBar;
 
-  const commandButton = ({ label, bgColor: backgroundColor, command }) => new TouchBarButton({
-    label,
-    backgroundColor,
-    click: () => {
-      win.sessions.get(currentUid).write(`\r${command}\r`);
-    }
-  });
+  const commandButton = ({ label, bgColor: backgroundColor, command }) =>
+    new TouchBarButton({
+      label,
+      backgroundColor,
+      click: () => {
+        win.sessions.get(currentUid).write(`\r${command}\r`);
+      }
+    });
 
-  const generateButtons = (view = 'default') => TOUCH_BAR_CONTROLS[view].map(control => commandButton(control));
+  const generateButtons = (view = 'default') =>
+    getTouchBarControls(view)
+      .map(control => {
+        if (control.type === 'button') {
+          return commandButton(control.payload);
+        }
+        return null;
+      })
+      .filter(control => control);
 
-  const touchBar = new TouchBar(generateButtons());
-
-  win.setTouchBar(touchBar);
+  win.setTouchBar(new TouchBar(generateButtons()));
 
   win.rpc.on('uid set', uid => {
     currentUid = uid;
+  });
+  win.rpc.on('enter pressed', () => {
+    setTimeout(() => {
+      win.setTouchBar(new TouchBar(generateButtons()));
+    }, 200);
   });
 };
 
@@ -51,9 +77,46 @@ exports.middleware = () => next => action => {
       window.rpc.emit('uid set', action.uid);
       break;
     }
+    case 'ENTER_PRESSED': {
+      window.rpc.emit('enter pressed');
+      break;
+    }
     default: {
       break;
     }
   }
   return next(action);
+};
+
+exports.decorateTerm = function (Term, { React }) {
+  return class extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+      this.onTerminal = this.onTerminal.bind(this);
+    }
+
+    onTerminal(term) {
+      if (this.props.onTerminal) {
+        this.props.onTerminal(term);
+      }
+
+      term.uninstallKeyboard();
+      const someHandler = ['keydown', e => {
+        if (e.keyCode === 13) {
+          store.dispatch({
+            type: 'ENTER_PRESSED'
+          });
+        }
+      }];
+      term.keyboard.handlers_ = [someHandler].concat(term.keyboard.handlers_);
+      term.installKeyboard();
+    }
+
+    render() {
+      const props = Object.assign({}, this.props, {
+        onTerminal: this.onTerminal
+      });
+      return React.createElement(Term, props);
+    }
+  };
 };
